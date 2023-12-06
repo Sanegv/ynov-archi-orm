255ccdac4f6c1d3d1f48cb607f08a6789a6f1803
Sanegv <43825023+Sanegv@users.noreply.github.com>
2023-12-06 21:36:29 +0100

    added user class

---
+++ src/User.java
@@ -0,0 +1,54 @@
+public class User {
+    private int id;
+    private String login;
+    private String email;
+    private String password;
+    
+    public int getId(){
+        return id;
+    }
+
+    public String getLogin(){
+        return login;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setLogin(String login){
+        this.login = login;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public User(String login, String email, String password){
+        setEmail(email);
+        setLogin(login);
+        setPassword(password);
+        this.id = 1;
+    }
+
+    public String toString(){
+        String str = "id: ";
+        str += Integer.toString(id);
+        str += ",\n login: ";
+        str += login;
+        str += ",\n email: ";
+        str += email;
+        str += ",\n password: ";
+        str += password;
+
+        return str;
+    }
+}
