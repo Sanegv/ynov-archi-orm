b2065705e03f27a3200682708b54ebc250806318
Quentin <quentin.lemaire@ynov.com>
2023-12-07 17:03:21 +0100

    request accepts any type

--- src/SqlOrm.java
+++ src/SqlOrm.java
@@ -17,17 +17,56 @@ public class SqlOrm {
         }
     }
 
-    public ArrayList<String> requestStrings(String table, String column, int ID) throws Exception{       
-        String req = "SELECT * from " + table + " WHERE ID=?";
+    public ArrayList<String> requestStrings(String table, String column, String field, String value) throws Exception{       
+        String req = "SELECT * from " + table + " WHERE " + field + "=?";
         ArrayList<String> result = new ArrayList<String>();
 
         try (PreparedStatement statement = connection.prepareStatement(req)) {
-            statement.setString(1, Integer.toString(ID));
+            statement.setString(1, value);
+            System.out.println(statement);
 
             try (ResultSet rs = statement.executeQuery()) {
                 while (rs.next()) {
                     String str = rs.getString(column);
                     result.add(str);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public ArrayList<Integer> requestInts(String table, String column, String field, String value) throws Exception{       
+        String req = "SELECT * from " + table + " WHERE " + field + "=?";
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        try (PreparedStatement statement = connection.prepareStatement(req)) {
+            statement.setString(1, value);
+            System.out.println(statement);
+
+            try (ResultSet rs = statement.executeQuery()) {
+                while (rs.next()) {
+                    int i = rs.getInt(column);
+                    result.add(i);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public <T> ArrayList<T> request(String table, String column, String field, String value) throws Exception{       
+        String req = "SELECT * from " + table + " WHERE " + field + "=?";
+        ArrayList<T> result = new ArrayList<T>();
+
+        try (PreparedStatement statement = connection.prepareStatement(req)) {
+            statement.setString(1, value);
+            System.out.println(statement);
+
+            try (ResultSet rs = statement.executeQuery()) {
+                while (rs.next()) {
+                    T res = (T)rs.getObject(column);
+                    result.add(res);
                 }
             }
         }
