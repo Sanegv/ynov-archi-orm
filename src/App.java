f360d4341867c9d94a9d3fc35f902ab2b321883d
Quentin <quentin.lemaire@ynov.com>
2023-12-21 12:16:49 +0100

    implemented some tests for the lib

--- src/App.java
+++ src/App.java
@@ -9,37 +9,24 @@ import java.util.Hashtable;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        /*System.out.println("Hello, World!");
-        String mail = "toto@gmail.com";        
-        String name = "toto";
-        int id = 1;
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "toto", "bob");
-        try (connexion) {
-            String req = "SELECT * from User WHERE id=?;";
-            try (PreparedStatement statement = connexion.prepareStatement(req)) {
-                statement.setString(1, Integer.toString(id));
-                try (ResultSet rs = statement.executeQuery()) {
-                    while (rs.next()) {
-                        String firstname = rs.getString("login");
-                        String email = rs.getString("email");
-                        String password = rs.getString("password");
-                        System.out.println("user.name = " + firstname);
-                        System.out.println("user.email =  " + email);                        
-                        System.out.println("user.password =  " + password);
-                        System.out.println("-----------------");
-                    }
-                }
-            }
-        }*/
 
         SqlOrm orm = new SqlOrm("jdbc:mysql://localhost:3306/testdb", "toto", "bob");
-        /*ArrayList<String> list = orm.request("User", "login","email", "toto@gmail.com");
+        ArrayList<String> list = orm.request("User", "login","email", "toto@gmail.com");
         for(int i = 0; i < list.size(); i++){
             System.out.println("login: " + list.get(i));
-        }*/
+        }
+
+        User u = new User(4, "test", "toto@gmail.com", "admin");
+        orm.insert("user", u);
         
-        User user = new User(4, "test", "test@gmail.com", "test");
-        orm.createTable(user);
+        list = orm.request("User", "login","email", "toto@gmail.com");
+        for(int i = 0; i < list.size(); i++){
+            System.out.println("login: " + list.get(i));
+        }
+
+        orm.deleteElementByID("user", 4);list = orm.request("User", "login","email", "toto@gmail.com");
+        for(int i = 0; i < list.size(); i++){
+            System.out.println("login: " + list.get(i));
+        }
     }
 }
